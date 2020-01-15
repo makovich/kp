801963259d60c206a63c11386222b081d2f26e4f
paul <paul@makovi.ch>
2020-01-16 00:34:42 +0300

    Keyring for Linux platform

--- src/keyring.rs
+++ src/keyring.rs
@@ -43,3 +43,125 @@ impl<'a> Keyring<'a> {
             .map_err(|e| format!("{}", e))
     }
 }
+
+#[cfg(target_os = "linux")]
+use libc::{c_char, c_int, syscall, SYS_add_key, SYS_keyctl, SYS_request_key};
+
+#[cfg(target_os = "linux")]
+use log::*;
+
+#[cfg(target_os = "linux")]
+pub struct Keyring {
+    desc: std::ffi::CString,
+}
+
+#[cfg(target_os = "linux")]
+impl Keyring {
+    pub fn new(keyname: &str, account: &str) -> Result<Self, String> {
+        let desc = [keyname, account].join(":").into_bytes();
+        let desc = unsafe { std::ffi::CString::from_vec_unchecked(desc) };
+
+        Ok(Keyring { desc })
+    }
+
+    pub fn get_password(&self) -> Result<Pwd, String> {
+        const KEYCTL_READ: c_int = 11;
+
+        let pwd = unsafe {
+            let err = Err("no key found in keyring".to_owned());
+
+            let key_id = match syscall(
+                SYS_request_key,
+                b"user\0", // type for user-defined keyrings
+                self.desc.as_ptr(),
+                std::ptr::null::<c_char>(),
+                0,
+            ) {
+                -1 => {
+                    info!("keyctl key decription: {:?}", self.desc);
+                    info!("errno: {:?}", std::io::Error::last_os_error());
+                    return err;
+                }
+                val => val,
+            };
+
+            let data_len = match syscall(
+                SYS_keyctl,
+                KEYCTL_READ,
+                key_id,
+                std::ptr::null::<c_char>(),
+                0,
+            ) {
+                -1 => {
+                    info!("errno: {:?}", std::io::Error::last_os_error());
+                    return err;
+                }
+                val => val,
+            };
+
+            let mut data = vec![0u8; data_len as usize];
+
+            if -1 == syscall(SYS_keyctl, KEYCTL_READ, key_id, data.as_mut_ptr(), data_len) {
+                info!("errno: {:?}", std::io::Error::last_os_error());
+                return err;
+            }
+
+            String::from_utf8_unchecked(data)
+        };
+
+        Ok(pwd.into())
+    }
+
+    pub fn set_password(&self, password: &str) -> Result<(), String> {
+        const KEY_SPEC_SESSION_KEYRING: c_int = -3;
+
+        unsafe {
+            if -1
+                == syscall(
+                    SYS_add_key,
+                    b"user\0",
+                    self.desc.as_ptr(),
+                    password.as_ptr(),
+                    password.len(),
+                    KEY_SPEC_SESSION_KEYRING,
+                )
+            {
+                info!("keyctl key decription: {:?}", self.desc);
+                info!("errno: {:?}", std::io::Error::last_os_error());
+                return Err("unable to store password in keyring".to_owned());
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn delete_password(&self) -> Result<(), String> {
+        const KEYCTL_INVALIDATE: c_int = 21;
+
+        unsafe {
+            let err = Err("unable to remove key from keyring".to_owned());
+
+            let key_id = match syscall(
+                SYS_request_key,
+                b"user\0", // type for user-defined keyrings
+                self.desc.as_ptr(),
+                std::ptr::null::<c_char>(),
+                0,
+            ) {
+                -1 => {
+                    info!("keyctl key decription: {:?}", self.desc);
+                    info!("errno: {:?}", std::io::Error::last_os_error());
+                    return err;
+                }
+                val => val,
+            };
+
+            if -1 == syscall(SYS_keyctl, KEYCTL_INVALIDATE, key_id) {
+                info!("errno: {:?}", std::io::Error::last_os_error());
+                return err;
+            }
+        }
+
+        Ok(())
+    }
+}
