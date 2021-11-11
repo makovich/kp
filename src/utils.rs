60d6702631c60c1bb5a433e8f97125c4b06198ca
paul <paul@makovi.ch>
2021-11-11 19:10:41 +0300

    Skim update

--- src/utils.rs
+++ src/utils.rs
@@ -5,7 +5,7 @@ use crate::STDIN;
 #[cfg(feature = "clipboard")]
 use clipboard::{ClipboardContext, ClipboardProvider};
 use kdbx4::{CompositeKey, Database, Entry, Kdbx4};
-use skim::{Skim, SkimOptions};
+use skim::prelude::*;
 
 use log::*;
 
@@ -97,42 +97,61 @@ pub fn skim<'a>(
     query: Option<&'a str>,
     hide_groups: bool,
 ) -> Option<&'a Entry<'a>> {
-    let opts = SkimOptions {
-        multi: true,
-        reverse: true,
-        query,
-        height: Some("7"),
-        bind: vec![
+    let opts = SkimOptionsBuilder::default()
+        .multi(false)
+        .reverse(true)
+        .query(query)
+        .height(Some("7"))
+        .bind(vec![
             "ctrl-q:ignore", // toggle interactive
             "ctrl-l:ignore", // clear screen
             "ctrl-r:ignore", // rotate mode
-        ],
-        delimiter: if hide_groups { None } else { Some("/") },
-        nth: if hide_groups { Some("-1") } else { None },
+        ])
+        .delimiter(if hide_groups { None } else { Some("/") })
+        .build()
+        .expect("well formed SkimOptions");
 
-        ..SkimOptions::default()
-    };
+    let (tx, rx): (SkimItemSender, SkimItemReceiver) = unbounded();
 
-    let input = entries
+    entries
         .iter()
-        .map(|e| {
-            if hide_groups {
-                e.title().to_string()
+        .enumerate()
+        .map(|(idx, e)| {
+            let title = if hide_groups {
+                e.title().to_owned()
             } else {
                 format!("/{}/{}", e.group(), e.title())
+            };
+            EntryItem { idx, title }
+        })
+        .for_each(|item| tx.send(Arc::new(item)).unwrap());
+
+    // No more input expected, dropping sender
+    drop(tx);
+
+    Skim::run_with(&opts, Some(rx))
+        .map(|res| {
+            if res.is_abort || res.selected_items.len() != 1 {
+                None
+            } else {
+                res.selected_items[0]
+                    .as_ref()
+                    .as_any()
+                    .downcast_ref::<EntryItem>()
+                    .map(|ei| &entries[ei.idx])
             }
         })
-        .collect::<Vec<String>>()
-        .join("\n");
+        .unwrap()
+}
 
-    let result = Skim::run_with(&opts, Some(Box::new(::std::io::Cursor::new(input))))
-        .map(|o| o.selected_items)
-        .unwrap_or_else(Vec::new);
+struct EntryItem {
+    idx: usize,
+    title: String,
+}
 
-    if let [item] = result.as_slice() {
-        Some(&entries[item.get_index()])
-    } else {
-        None
+impl SkimItem for EntryItem {
+    fn text(&self) -> Cow<str> {
+        Cow::Borrowed(&self.title)
     }
 }
 
